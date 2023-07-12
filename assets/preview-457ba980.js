d698071d4166dab9972bca19d1cfdc6e501588cf
Thinh Nguyen <ducthinh.nguyen@diag.vn>
2023-07-12 23:52:56 +0700

    ...

---
+++ assets/preview-457ba980.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-15b5c827.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-aabb7931.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-aabb7931.js","./iframe-15b5c827.js","./_commonjsHelpers-725317a4.js","./index-d475d2ea.js","./index-d37d4223.js","./index-826364cc.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-457ba980.js.map
