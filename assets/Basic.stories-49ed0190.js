17a764d47952425f2e2d7b55bfe05604b67ac972
Thinh Nguyen <ngphanducthinh@gmail.com>
2023-11-19 13:23:26 +0000

    Update built files

---
+++ assets/Basic.stories-49ed0190.js
@@ -0,0 +1,47 @@
+import{_ as i}from"./BTextarea-301e3dec.js";import{_ as l}from"./BTextField-60ad201b.js";import{b as d}from"./vue.esm-bundler-f0a4de28.js";import{_ as s}from"./FormValidationExample-eeac0973.js";import"./Validation-4b68e7d0.js";import"./Common-43134899.js";import"./vue-i18n.esm-bundler-45575b10.js";import"./BErrorMessage-4230966b.js";import"./BLabel-0c4df10b.js";import"./v4-a960c1f4.js";import"./BButton-303b33a5.js";import"./Enums-693cd203.js";const T={title:"FormValidation/Basic",component:s,tags:["autodocs"],args:{}},a={render:m=>({components:{FormValidationExample:s,BTextField:l,BTextarea:i},setup(){const o=d({firstName:"",lastName:"",message:""});return{args:m,formData:o}},template:`
+      <FormValidationExample v-bind="args">
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.firstName" label="First name" required />
+        </div>
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.lastName" label="Last name" required />
+        </div>
+        <div class="ds-w-full">
+          <BTextarea v-model="formData.message" label="Message" required />
+        </div>
+      </FormValidationExample>
+    `}),args:{}};var e,r,t;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: any) => ({
+    components: {
+      FormValidationExample,
+      BTextField,
+      BTextarea
+    },
+    setup() {
+      const formData = ref({
+        firstName: '',
+        lastName: '',
+        message: ''
+      });
+      return {
+        args,
+        formData
+      };
+    },
+    template: \`
+      <FormValidationExample v-bind="args">
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.firstName" label="First name" required />
+        </div>
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.lastName" label="Last name" required />
+        </div>
+        <div class="ds-w-full">
+          <BTextarea v-model="formData.message" label="Message" required />
+        </div>
+      </FormValidationExample>
+    \`
+  }),
+  args: {}
+}`,...(t=(r=a.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const b=["Default"];export{a as Default,b as __namedExportsOrder,T as default};
+//# sourceMappingURL=Basic.stories-49ed0190.js.map
---
+++ assets/preview-98abd561.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-2e27e629.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-442c2285.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-442c2285.js","./iframe-2e27e629.js","./_commonjsHelpers-725317a4.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-9f656a0c.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-98abd561.js.map
