7645d3dddfe1f5e6bc44bf9b3107deeb70100817
Thinh Nguyen <ngphanducthinh@gmail.com>
2024-01-10 03:03:16 +0000

    Update built files

---
+++ assets/Basic.stories-f5ceb379.js
@@ -0,0 +1,46 @@
+import{_ as n}from"./BTextarea-a5695a53.js";import{B as i}from"./BTextField-4bf88596.js";import{e as l}from"./vue.esm-bundler-29df2e62.js";import{_ as s}from"./FormValidationExample-effe9173.js";import"./Validation-5669da51.js";import"./Common-43134899.js";import"./vue-i18n.esm-bundler-152c8982.js";import"./BErrorMessage-f3a23431.js";import"./BLabel-57c6859e.js";import"./v4-a960c1f4.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./BButton-f328baed.js";import"./Enums-5b02df9b.js";const N={title:"FormValidation/Basic",component:s,tags:["autodocs"],args:{}},a={render:m=>({components:{FormValidationExample:s,BTextField:i,BTextarea:n},setup(){const o=l({firstName:"",lastName:"",message:""});return{args:m,formData:o}},template:`
+      <FormValidationExample v-bind="args">
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.firstName" label="First name" required />
+        </div>
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.lastName" label="Last name" required />
+        </div>
+        <div class="ds-w-full">
+          <BTextarea v-model="formData.message" label="Message" required />
+        </div>
+      </FormValidationExample>
+    `}),args:{}};var e,r,t;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: any) => ({
+    components: {
+      FormValidationExample,
+      BTextField,
+      BTextarea
+    },
+    setup() {
+      const formData = ref({
+        firstName: '',
+        lastName: '',
+        message: ''
+      });
+      return {
+        args,
+        formData
+      };
+    },
+    template: \`
+      <FormValidationExample v-bind="args">
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.firstName" label="First name" required />
+        </div>
+        <div class="ds-flex-1">
+          <BTextField v-model="formData.lastName" label="Last name" required />
+        </div>
+        <div class="ds-w-full">
+          <BTextarea v-model="formData.message" label="Message" required />
+        </div>
+      </FormValidationExample>
+    \`
+  }),
+  args: {}
+}`,...(t=(r=a.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const _=["Default"];export{a as Default,_ as __namedExportsOrder,N as default};
---
+++ assets/WithTooltip-V3YHNWJZ-ff9d798a.js
@@ -0,0 +1,1 @@
+import{W as e,W as h,a as l}from"./DocsRenderer-NNNQARDV-289f7460.js";import"./iframe-92198267.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-11d98b33.js";import"./index-41dadfd5.js";import"./index-356e4a49.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
