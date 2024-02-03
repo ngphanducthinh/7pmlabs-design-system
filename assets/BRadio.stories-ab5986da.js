3341dd4520954194b96306e3dda22708de7bb963
Thinh Nguyen <ngphanducthinh@gmail.com>
2024-02-03 14:27:53 +0000

    Update built files

---
+++ assets/BRadio.stories-ab5986da.js
@@ -0,0 +1,52 @@
+import{j as x,k as d,o,b as m,l as p,w as _,I as N,n as f,t as S,m as c,x as D,u as k,F as I,e as M}from"./vue.esm-bundler-29df2e62.js";import{_ as w}from"./BErrorMessage-f3a23431.js";import{u as E}from"./Validation-8439caab.js";import{u as F}from"./vue-i18n.esm-bundler-152c8982.js";import{v as $}from"./v4-a960c1f4.js";import"./Common-c3b475f1.js";const A={class:"ds-flex ds-items-center"},U=["id","disabled","name","value"],j=["for"],i=x({__name:"BRadio",props:{inputId:{default:""},modelValue:{type:[String,Number,Boolean],default:""},inputName:{default:""},label:{default:""},disabled:{type:Boolean,default:!1},labelCssClass:{default:""},hideDetails:{type:Boolean,default:!1},validationRules:{default:void 0},required:{type:Boolean,default:!1},requiredErrorMessage:{default:""}},emits:["change","update:modelValue"],setup(n,{emit:l}){const a=n,{t:y}=F(),V={validateRule:e=>!!(e&&e.trim()),errorMessage:()=>a.requiredErrorMessage||y("ds.global.field_required")},t=d(()=>a.inputId||`id-${$()}`),r=d({get(){return a.modelValue},set(e){l("update:modelValue",e)}}),R=d(()=>{let e=[];return a.required&&e.push(V),a.validationRules&&(e=e.concat(a.validationRules)),e.length?e:void 0}),{validate:q,validationResult:h}=E(t.value,r,R.value),B=()=>{l("change"),q()};return(e,u)=>(o(),m(I,null,[p("div",A,[_(p("input",{id:t.value,"onUpdate:modelValue":u[0]||(u[0]=C=>r.value=C),class:f([{"ds-cursor-not-allowed":e.disabled},"ds-h-6 ds-min-h-[1.5rem] ds-w-6 ds-min-w-[1.5rem] ds-border-gray-300 ds-bg-gray-100 ds-text-primary-t focus:ds-ring-primary-t"]),disabled:e.disabled,name:e.inputName,value:e.$attrs.value,type:"radio",onChange:B},null,42,U),[[N,r.value]]),e.label?(o(),m("label",{key:0,class:f([e.labelCssClass,"ds-ml-2 ds-text-sm ds-font-medium ds-text-gray-900"]),for:t.value},S(e.label),11,j)):c("",!0)]),e.hideDetails?c("",!0):(o(),D(w,{key:0,"error-message":k(h).errorMessage(),class:"ds-mt-1"},null,8,["error-message"]))],64))}});i.__docgenInfo={exportName:"default",displayName:"BRadio",description:"",tags:{},props:[{name:"inputId",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"''"}},{name:"modelValue",required:!0,type:{name:"union",elements:[{name:"string"},{name:"number"},{name:"boolean"}]},defaultValue:{func:!1,value:"''"}},{name:"inputName",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"''"}},{name:"label",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"''"}},{name:"disabled",required:!1,type:{name:"boolean"},defaultValue:{func:!1,value:"false"}},{name:"labelCssClass",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"''"}},{name:"hideDetails",description:"Hide the validation error message.",required:!1,type:{name:"boolean"},defaultValue:{func:!1,value:"false"}},{name:"validationRules",description:"Array of custom validation rules.",required:!1,type:{name:"Array",elements:[{name:"ValidationRule"}]},defaultValue:{func:!1,value:"undefined"}},{name:"required",description:"Validate if the field is left empty.",required:!1,type:{name:"boolean"},defaultValue:{func:!1,value:"false"}},{name:"requiredErrorMessage",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"''"}}],events:[{name:"change"},{name:"update:modelValue",type:{names:["union"],elements:[{name:"string"},{name:"number"},{name:"boolean"}]},description:"Update value, param: <code>value: string | number | boolean</code>",properties:[{type:{names:["mixed"]},description:"e"},{type:{names:["mixed"]},name:"value"}],tags:[{title:"param",type:{name:"mixed"},description:"e"},{title:"param",type:{name:"mixed"},name:"value"}]}],sourceFiles:["/home/runner/work/design-system/design-system/src/components/BRadio.vue"]};const K={title:"Components/Radio",parameters:{docs:{description:{component:"The <code>BRadio</code> component is a simple radio button."}}},component:i,tags:["autodocs"],args:{inputId:"",inputName:"",label:"",disabled:!1,labelCssClass:"",hideDetails:!1,validationRules:void 0,required:!1,requiredErrorMessage:""}},s={render:n=>({components:{BRadio:i},setup(){const l=M("");return{args:n,radio:l}},template:`
+      <BRadio
+        v-bind="args"
+        v-model="radio"
+        value="Value 01"
+        label="Select 'Value 01'"
+      />
+      <BRadio
+        v-bind="args"
+        v-model="radio"
+        value="Value 02"
+        label="Select 'Value 02'"
+      />
+      
+      <p class="ds-text-xs ds-text-gray-500">
+        Selected value: {{ radio }}
+      </p>
+    `}),args:{modelValue:""}};var v,g,b;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: (args: any) => ({
+    components: {
+      BRadio
+    },
+    setup() {
+      const radio = ref('');
+      return {
+        args,
+        radio
+      };
+    },
+    template: \`
+      <BRadio
+        v-bind="args"
+        v-model="radio"
+        value="Value 01"
+        label="Select 'Value 01'"
+      />
+      <BRadio
+        v-bind="args"
+        v-model="radio"
+        value="Value 02"
+        label="Select 'Value 02'"
+      />
+      
+      <p class="ds-text-xs ds-text-gray-500">
+        Selected value: {{ radio }}
+      </p>
+    \`
+  }),
+  args: {
+    modelValue: ''
+  }
+}`,...(b=(g=s.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};const L=["Default"];export{s as Default,L as __namedExportsOrder,K as default};
---
+++ assets/Introduction-e2c1e482.js
@@ -0,0 +1,36 @@
+import{r as h}from"./index-c013ead5.js";import{u as d}from"./index-dc1d5b46.js";import"./_commonjsHelpers-725317a4.js";var a={exports:{}},o={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var u=h,x=Symbol.for("react.element"),f=Symbol.for("react.fragment"),j=Object.prototype.hasOwnProperty,y=u.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,b={key:!0,ref:!0,__self:!0,__source:!0};function m(r,e,c){var s,t={},i=null,l=null;c!==void 0&&(i=""+c),e.key!==void 0&&(i=""+e.key),e.ref!==void 0&&(l=e.ref);for(s in e)j.call(e,s)&&!b.hasOwnProperty(s)&&(t[s]=e[s]);if(r&&r.defaultProps)for(s in e=r.defaultProps,e)t[s]===void 0&&(t[s]=e[s]);return{$$typeof:x,type:r,key:i,ref:l,props:t,_owner:y.current}}o.Fragment=f;o.jsx=m;o.jsxs=m;a.exports=o;var n=a.exports;function p(r){const e=Object.assign({h1:"h1",a:"a",p:"p",code:"code",h2:"h2",pre:"pre"},d(),r.components);return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{id:"7pmlabs-design-system",children:n.jsx(e.a,{href:"https://www.npmjs.com/package/@7pmlabs/design-system",target:"_blank",rel:"nofollow noopener noreferrer",children:"7PMLabs Design System"})}),`
+`,n.jsx(e.p,{children:"UI Library with beautifully handcrafted Vue Components."}),`
+`,n.jsxs(e.p,{children:["In case, you also use ",n.jsx(e.a,{href:"https://tailwindcss.com/docs/installation",target:"_blank",rel:"nofollow noopener noreferrer",children:"Tailwind CSS"})," in your project, do not import ",n.jsx(e.code,{children:"@import 'tailwindcss/base';"})," unless it causes broken styles."]}),`
+`,n.jsx(e.h2,{id:"getting-started",children:"Getting started"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`npm i @7pmlabs/design-system
+`})}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.p,{children:"Import CSS & plugin in main.ts:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`import '@7pmlabs/design-system/style.css';
+
+const app = createApp(App);
+...
+app.use(ds, { i18n });
+`})}),`
+`,n.jsx(e.p,{children:"Import & use component:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`import { BButton } from '@7pmlabs/design-system';
+
+<BButton>Click me!</BButton>
+`})}),`
+`,n.jsx(e.h2,{id:"color-customization",children:"Color Customization"}),`
+`,n.jsx(e.p,{children:"CSS Variables:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`--ds-colors-primary-f: 6 182 212; // #06b6d4
+--ds-colors-primary-t: 59 130 246; // #3b82f6
+--ds-colors-focus: 86 221 214; // #56ddd6
+
+--ds-colors-primary-f-stop: 0%;
+--ds-colors-primary-t-stop: 100%;
+`})})]})}function C(r={}){const{wrapper:e}=Object.assign({},d(),r.components);return e?n.jsx(e,Object.assign({},r,{children:n.jsx(p,r)})):p(r)}export{C as default};
